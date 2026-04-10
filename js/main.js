79aea2adb33783b011dfd8cca8b1ce3a64d9f514
Nick Lewis <thesustainable.dev@gmail.com>
2026-04-10 15:53:37 +0100

    Tweaks and bug fixes
    - Remove and replace JS highlight with simpler CSS only version
    - Adjust sr-only class as chrome was showing a 1px white element in the footer
    - Update editor styles to make it clear where highlights are used

--- js/main.js
+++ js/main.js
@@ -1,25 +1,24 @@
 import initNavigation from './components/navigation.js';
 import initCarousel from './components/carousel.js';
-import { annotate } from 'rough-notation';
+// import { annotate } from 'rough-notation';
 
 document.addEventListener("DOMContentLoaded", () => {
 
 	initNavigation();
 	initCarousel();
 
-	const elements = document.querySelectorAll('.annotate');
-	elements.forEach(el => {
-		const annotation = annotate(el, {
-			type: 'highlight',
-			color: '#CBFFCB',
-			animate: false,
-			multiline: true,
-			iterations: 1,
-			padding: [0, 2]
-		});
-		annotation.show();
-	});
-
+	// const elements = document.querySelectorAll('.annotate');
+	// elements.forEach(el => {
+	// 	const annotation = annotate(el, {
+	// 		type: 'highlight',
+	// 		color: '#CBFFCB',
+	// 		animate: false,
+	// 		multiline: true,
+	// 		iterations: 1,
+	// 		padding: [0, 2]
+	// 	});
+	// 	annotation.show();
+	// });
 
 	const progressBar = document.querySelector('.progress-bar-inner');
 	function updateProgress() {
